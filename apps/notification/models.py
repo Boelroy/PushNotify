c8c15d10ca2351c1c45e2a9aed8ef990087be14a
boelroy <boelroy@live.com>
2013-09-02 22:26:06 +0800

    add the class Offlinemsg to cache the unpushed msg

--- apps/notification/models.py
+++ apps/notification/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-	"""docstring for Users"""
+	"""the Model of the Notification Users"""
 	name = models.CharField(max_length=30)
 	lastonLineTime = models.DateTimeField()
 	onlineStatus = models.BooleanField()
 	ip = models.IPAddressField()
+
+
+
+class OffLineNotification(models.Model):
+	""" Model of Notification that no 
+		pushed to the user
+	"""
+	note = models.CharField(max_length=7000)
+	notifiUsername = models.CharField(max_length=30)
+
 	
