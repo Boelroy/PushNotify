bba533266154a640d07df58b0cff47291f3f9bf3
boelroy <boelroy@live.com>
2013-09-02 22:23:04 +0800

    this is the user helper class to define some ops of Users entity

---
+++ apps/notification/UserUtil.py
@@ -0,0 +1,60 @@
+from models import Users
+
+"""the singleton func"""
+def singleton(cls, *args, **kw):   
+    instances = {}   
+    def _singleton():   
+        if cls not in instances:   
+            instances[cls] = cls(*args, **kw)   
+        return instances[cls]   
+    return _singleton
+
+@singleton
+class UserUtil(object):
+	"""the User Helper class to 
+		to help Manage the User Entity
+	"""
+	def __init__(self):
+		pass
+	
+
+	def addUser(self,name,lastonLineTime,onlineStatus,ip):
+		existUser = Users.objects.filter(name=name)
+		if len(existUser) != 0:
+			raise Exception("the Users is already exits")
+
+		newUser = Users(name=name, 
+						ip=ip, 
+						onlineStatus = True, 
+						lastonLineTime=lastonLineTime)
+
+		newUser.save()
+
+	def changeUser(self, name, **changeDic):
+		existUser = self.getUsers(name)
+		changeDic = dict(changeDic)
+		print changeDic
+		if existUser:
+			for attribute, value in changeDic.iteritems():
+				print attribute
+				print "in set attr " + attribute + "and the value is" + str(value)
+				setattr(existUser, attribute, value)
+				print attribute + str(getattr(existUser, attribute))
+
+			print "it is saving the user"
+			existUser.save()
+			print str(existUser.onlineStatus)
+
+	def isInUsers(self, name):
+		try:
+			existUser = Users.objects.get(name=name)
+			return True
+		except Exception, e:
+			return False
+
+	def getUsers(self, name):
+		try:
+			existUser = Users.objects.get(name=name)
+			return existUser
+		except Exception, e:
+			return None
