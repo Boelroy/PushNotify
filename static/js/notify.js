0e9cf2cf060193bb7e8957f904f5c37c0788ede2
boelroy <boelroy@live.com>
2013-09-02 22:20:21 +0800

    change the js of the web

--- static/js/notify.js
+++ static/js/notify.js
@@ -12,21 +12,3 @@ $('#submit').click(function(){
 	})
 });
 
-$('ul.nav li a').click(function(){
-	var active_one = this;
-	var action_type = $(this).attr('title');
-	$.ajax({
-		url: '/'+action_type,
-		data: {'action':action_type},
-		type: 'get',
-		success: function(data){
-			$.getScript("static/js/users.js")
-			$.each($('ul.nav li'), function(){
-				$(this).removeClass('active');
-			});
-			$(active_one).parent().addClass('active');
-			$('section').replaceWith(data)
-			$.getScript("static/js/notify.js")
-		}
-	})
-});
